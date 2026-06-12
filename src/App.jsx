2c97f602a7f822265e0c16c8f70b1da2c71aaf0b
OlgaStarchenko <o9915530@gmail.com>
2026-06-12 18:47:55 +1000

    The layout and CSS styles for the validation form have been written.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import styles from "./app.module.css";
 
 export function App() {
-  return <p>Hello!</p>;
+  return (
+    <div className={styles.container}>
+      <form className={styles.validationForm}>
+        <input className={styles.validationForm__input} />
+        <div className={styles.errorLabel}>Error</div>
+        <input className={styles.validationForm__input} />
+        <div className={styles.errorLabel}>Error</div>
+        <input className={styles.validationForm__input} />
+        <div className={styles.errorLabel}>Error</div>
+        <button className={styles.validationForm__btn}>
+          Зарегистрироваться
+        </button>
+      </form>
+    </div>
+  );
 }
