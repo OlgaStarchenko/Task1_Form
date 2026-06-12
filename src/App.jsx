6a01ed929e9e95afb07ed0baaee7328d245057fc
OlgaStarchenko <o9915530@gmail.com>
2026-06-12 19:25:31 +1000

    The useState hook was imported, the formData state was added to store form data, its properties were destructured, and the form field and button attributes were configured. The form fields were made controllable. The handleChange handler was implemented to update the state when an input value was provided, and the sendData function was implemented to output form data to the console. The newFormData object was added to store data between renders.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,66 @@
+import { useState } from "react";
 import styles from "./app.module.css";
 
 export function App() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const { email, password, repeatPassword } = formData;
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    const newFormData = {
+      ...formData,
+      [name]: value,
+    };
+    setFormData(newFormData);
+  };
+
+  const sendData = (event) => {
+    event.preventDefault();
+    console.log(formData);
+
+    setFormData({ email: "", password: "", repeatPassword: "" });
+  };
+
   return (
     <div className={styles.container}>
-      <form className={styles.validationForm}>
-        <input className={styles.validationForm__input} />
+      <form className={styles.validationForm} onSubmit={sendData}>
+        <input
+          className={styles.validationForm__input}
+          name="email"
+          type="email"
+          value={email}
+          onChange={handleChange}
+        />
+
         <div className={styles.errorLabel}>Error</div>
-        <input className={styles.validationForm__input} />
+
+        <input
+          className={styles.validationForm__input}
+          name="password"
+          type="password"
+          value={password}
+          onChange={handleChange}
+        />
+
         <div className={styles.errorLabel}>Error</div>
-        <input className={styles.validationForm__input} />
+
+        <input
+          className={styles.validationForm__input}
+          name="repeatPassword"
+          type="password"
+          value={repeatPassword}
+          onChange={handleChange}
+        />
+
         <div className={styles.errorLabel}>Error</div>
-        <button className={styles.validationForm__btn}>
+
+        <button className={styles.validationForm__btn} type="submit">
           Зарегистрироваться
         </button>
       </form>
